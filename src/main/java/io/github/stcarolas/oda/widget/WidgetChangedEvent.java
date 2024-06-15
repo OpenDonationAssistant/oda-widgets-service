0871540cc5fb715f676b47f255b2dba96a344a0f
stCarolas <stcarolas@gmail.com>
2024-06-15 14:43:22 +0300

    add logging for sent events

--- src/main/java/io/github/stcarolas/oda/widget/WidgetChangedEvent.java
+++ src/main/java/io/github/stcarolas/oda/widget/WidgetChangedEvent.java
@@ -21,4 +21,39 @@ public class WidgetChangedEvent {
   public Widget getWidget() {
     return widget;
   }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    result = prime * result + ((widget == null) ? 0 : widget.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    WidgetChangedEvent other = (WidgetChangedEvent) obj;
+    if (type == null) {
+      if (other.type != null) return false;
+    } else if (!type.equals(other.type)) return false;
+    if (widget == null) {
+      if (other.widget != null) return false;
+    } else if (!widget.equals(other.widget)) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "{\"_type\"=\"WidgetChangedEvent\",\"type\"=\"" +
+      type +
+      "\", widget\"=\"" +
+      widget +
+      "}"
+    );
+  }
 }
--- src/main/java/io/github/stcarolas/oda/widget/WidgetChangedNotificationSender.java
+++ src/main/java/io/github/stcarolas/oda/widget/WidgetChangedNotificationSender.java
@@ -2,8 +2,18 @@ package io.github.stcarolas.oda.widget;
 
 import io.micronaut.rabbitmq.annotation.Binding;
 import io.micronaut.rabbitmq.annotation.RabbitClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RabbitClient(WidgetRabbitConfiguration.CHANGES_WIDGETS)
 public interface WidgetChangedNotificationSender {
-  void send(@Binding String binding, WidgetChangedEvent event);
+  static Logger log = LoggerFactory.getLogger(
+    WidgetChangedNotificationSender.class
+  );
+  void _send(@Binding String binding, WidgetChangedEvent event);
+
+  public default void send(String binding, WidgetChangedEvent event) {
+    log.info("Send event: {}", event);
+    this._send(binding, event);
+  }
 }
