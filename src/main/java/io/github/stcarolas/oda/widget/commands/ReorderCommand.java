8ee3f44b385fe79a473937e4402ba9ccb028b412
stCarolas <stcarolas@gmail.com>
2025-01-21 08:27:24 +0300

    add logs

--- src/main/java/io/github/stcarolas/oda/widget/commands/ReorderCommand.java
+++ src/main/java/io/github/stcarolas/oda/widget/commands/ReorderCommand.java
@@ -2,11 +2,16 @@ package io.github.stcarolas.oda.widget.commands;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.github.stcarolas.oda.widget.WidgetRepository;
 import io.micronaut.serde.annotation.Serdeable;
 
 @Serdeable
 public class ReorderCommand {
+
+  private Logger log = LoggerFactory.getLogger(ReorderCommand.class);
 
   public List<String> ids;
 
@@ -18,10 +23,12 @@ public class ReorderCommand {
     var widgets = repository.find(ownerId);
     var updatedWidgets = widgets.stream().map(it -> {
       var order = ids.indexOf(it.getId());
+      log.debug("id: {}, order: {}", it.getId(), order);
       // TODO: immutable
       it.setSortOrder(order > 0 ? order : widgets.size());
       return it;
     }).toList();
+    log.debug("updated widgets: {}", updatedWidgets.stream().map(it -> it.getId()).toList());
     repository.updateAll(updatedWidgets);
   }
 
