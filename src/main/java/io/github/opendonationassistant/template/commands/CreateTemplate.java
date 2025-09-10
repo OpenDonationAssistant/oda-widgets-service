ff71f1cd1cca1c71c84d7b593b90857359bff028
stCarolas <stcarolas@gmail.com>
2025-09-10 14:06:56 +0300

    refack createtemplate command

--- src/main/java/io/github/opendonationassistant/template/commands/CreateTemplate.java
+++ src/main/java/io/github/opendonationassistant/template/commands/CreateTemplate.java
@@ -1,29 +1,52 @@
 package io.github.opendonationassistant.template.commands;
 
+import io.github.opendonationassistant.commons.micronaut.BaseController;
 import io.github.opendonationassistant.template.repository.TemplateRepository;
+import io.github.opendonationassistant.template.view.TemplateDto;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.authentication.Authentication;
+import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.inject.Inject;
 import java.util.List;
 import java.util.Map;
 
-@Serdeable
-public class CreateTemplate {
+@Controller
+public class CreateTemplate extends BaseController {
 
-  private String widgetType;
-  private String showcase;
-  private List<Map<String, Object>> properties;
+  private TemplateRepository repository;
 
-  public CreateTemplate(
+  @Inject
+  public CreateTemplate(TemplateRepository repository) {
+    this.repository = repository;
+  }
+
+  @Post("/templates/commands/create")
+  @Secured(SecurityRule.IS_AUTHENTICATED)
+  public HttpResponse<TemplateDto> execute(
+    Authentication auth,
+    CreateTemplateCommand command
+  ) {
+    var ownerId = getOwnerId(auth);
+    if (ownerId.isEmpty()) {
+      return HttpResponse.unauthorized();
+    }
+    repository.create(
+      ownerId.get(),
+      command.widgetType(),
+      command.showcase(),
+      command.properties()
+    );
+    return HttpResponse.ok();
+  }
+
+  @Serdeable
+  public static record CreateTemplateCommand(
     String widgetType,
     String showcase,
     List<Map<String, Object>> properties
-  ) {
-    this.widgetType = widgetType;
-    this.showcase = showcase;
-    this.properties = properties;
-  }
-
-  public void execute(String recipientId, TemplateRepository repository) {
-    repository.create(recipientId, widgetType, showcase, properties);
-  }
+  ) {}
 }
