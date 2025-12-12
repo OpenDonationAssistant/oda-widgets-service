3cd2644d5479793a0dfbe78dec268997c6556691
stCarolas <stcarolas@gmail.com>
2025-12-12 03:20:02 +0300

    left casting only alignment and font

--- src/main/java/io/github/opendonationassistant/widget/model/WidgetProperty.java
+++ src/main/java/io/github/opendonationassistant/widget/model/WidgetProperty.java
@@ -81,19 +81,19 @@ public class WidgetProperty<T> {
 
   public static WidgetProperty<?> of(String name, Object value) {
     return switch (name) {
-      case "alerts" -> new PaymentAlertProperty(name, value);
-      case "goal" -> new DonationGoalProperty(name, value);
-      case "labelTemplate" -> new DonationGoalLabelProperty(name, value);
+      // case "alerts" -> new PaymentAlertProperty(name, value);
+      // case "goal" -> new DonationGoalProperty(name, value);
+      // case "labelTemplate" -> new DonationGoalLabelProperty(name, value);
       // case "layout" -> new DonatersTopListLayoutProperty(name, value); //toplist but conflicts with paymentalerts
-      case "carousel" -> new DonatersTopListCarouselProperty(name, value);
-      case "price" -> new DonatonPriceProperty(name, value);
-      case "reelWinningEffect" -> new ReelWinningEffectProperty(name, value);
-      case "optionList" -> new ReelItemListProperty(name, value);
-      case "ReelItemBackgroundProperty" -> new ReelItemBackgroundProperty(
-        name,
-        value
-      );
-      case "roulette-items" -> new RouletteItemsProperty(name, value);
+      // case "carousel" -> new DonatersTopListCarouselProperty(name, value);
+      // case "price" -> new DonatonPriceProperty(name, value);
+      // case "reelWinningEffect" -> new ReelWinningEffectProperty(name, value);
+      // case "optionList" -> new ReelItemListProperty(name, value);
+      // case "ReelItemBackgroundProperty" -> new ReelItemBackgroundProperty(
+      //   name,
+      //   value
+      // );
+      // case "roulette-items" -> new RouletteItemsProperty(name, value);
       case "headerFont" -> new FontProperty(name, value); //goal
       case "font" -> new FontProperty(name, value); //goal
       case "titleFont" -> new FontProperty(name, value); //goal
@@ -105,115 +105,115 @@ public class WidgetProperty<T> {
       case "eventsAmountFont" -> new FontProperty(name, value); //horizontalevents
       case "requesterFont" -> new FontProperty(name, value); //player-info
       case "queueFont" -> new FontProperty(name, value); //player-info
-      case "width" -> new WidthProperty(name, value); //goal
-      case "headerWidth" -> new WidthProperty(name, value); //toplist
-      case "listWidth" -> new WidthProperty(name, value); //toplist
-      case "height" -> new HeightProperty(name, value); //goal
-      case "headerHeight" -> new HeightProperty(name, value); //toplist
-      case "listHeight" -> new HeightProperty(name, value); //toplist
-      case "outerHeight" -> new HeightProperty(name, value); //goal
-      case "filledHeight" -> new HeightProperty(name, value); //goal
-      case "widgetBackgroundColor" -> new BackgroundColorProperty(name, value); //goal
-      case "titleBackgroundColor" -> new BackgroundColorProperty(name, value); //goal
-      case "titleBackground" -> new BackgroundColorProperty(name, value); //player-info
-      case "listBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
-      case "backgroundColor" -> new BackgroundColorProperty(name, value); //goal
-      case "filledColor" -> new BackgroundColorProperty(name, value); //goal
-      case "itemBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
-      case "headerBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
-      case "eventsBackgroundColor" -> new BackgroundColorProperty(name, value); //horizontalevents
-      case "lineBackgroundColor" -> new BackgroundColorProperty(name, value); //horizontalevents
-      case "selectionColor" -> new BackgroundColorProperty(name, value); //horizontalevents
-      case "background" -> new BackgroundColorProperty(name, value); //horizontalevents
-      case "requesterBackground" -> new BackgroundColorProperty(name, value); //player-info
-      case "queueBackground" -> new BackgroundColorProperty(name, value); //player-info
+      // case "width" -> new WidthProperty(name, value); //goal
+      // case "headerWidth" -> new WidthProperty(name, value); //toplist
+      // case "listWidth" -> new WidthProperty(name, value); //toplist
+      // case "height" -> new HeightProperty(name, value); //goal
+      // case "headerHeight" -> new HeightProperty(name, value); //toplist
+      // case "listHeight" -> new HeightProperty(name, value); //toplist
+      // case "outerHeight" -> new HeightProperty(name, value); //goal
+      // case "filledHeight" -> new HeightProperty(name, value); //goal
+      // case "widgetBackgroundColor" -> new BackgroundColorProperty(name, value); //goal
+      // case "titleBackgroundColor" -> new BackgroundColorProperty(name, value); //goal
+      // case "titleBackground" -> new BackgroundColorProperty(name, value); //player-info
+      // case "listBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
+      // case "backgroundColor" -> new BackgroundColorProperty(name, value); //goal
+      // case "filledColor" -> new BackgroundColorProperty(name, value); //goal
+      // case "itemBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
+      // case "headerBackgroundColor" -> new BackgroundColorProperty(name, value); //toplist
+      // case "eventsBackgroundColor" -> new BackgroundColorProperty(name, value); //horizontalevents
+      // case "lineBackgroundColor" -> new BackgroundColorProperty(name, value); //horizontalevents
+      // case "selectionColor" -> new BackgroundColorProperty(name, value); //horizontalevents
+      // case "background" -> new BackgroundColorProperty(name, value); //horizontalevents
+      // case "requesterBackground" -> new BackgroundColorProperty(name, value); //player-info
+      // case "queueBackground" -> new BackgroundColorProperty(name, value); //player-info
       // case "backgroundImage" -> new BackgroundImageProperty(name, value); //goal, conflicts with reel
-      case "titleBackgroundImage" -> new BackgroundImageProperty(name, value); //goal
-      case "headerBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
-      case "outerImage" -> new BackgroundImageProperty(name, value); //goal
-      case "innerImage" -> new BackgroundImageProperty(name, value); //goal
-      case "listBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
-      case "itemBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
-      case "eventsBackgroundImage" -> new BackgroundImageProperty(name, value); //horizontalevents
-      case "lineBackgroundImage" -> new BackgroundImageProperty(name, value); //horizontalevents
-      case "border" -> new BorderProperty(name, value); //goal
-      case "titleBorder" -> new BorderProperty(name, value); //goal
-      case "itemBorder" -> new BorderProperty(name, value); //toplist
-      case "headerBorder" -> new BorderProperty(name, value); //toplist
-      case "outerBorder" -> new BorderProperty(name, value); //goal
-      case "innerBorder" -> new BorderProperty(name, value); //goal
-      case "widgetBorder" -> new BorderProperty(name, value); //toplist
-      case "listBorder" -> new BorderProperty(name, value); //toplist
-      case "eventsBorder" -> new BorderProperty(name, value); //toplist
-      case "lineBorder" -> new BorderProperty(name, value); //horizontalevents
-      case "cardBorder" -> new BorderProperty(name, value); //reel
-      case "requesterBorder" -> new BorderProperty(name, value); //player-info
-      case "queueBorder" -> new BorderProperty(name, value); //player-info
-      case "padding" -> new PaddingProperty(name, value); //goal
-      case "titlePadding" -> new PaddingProperty(name, value); //goal
-      case "itemPadding" -> new PaddingProperty(name, value); //toplist
-      case "headerPadding" -> new PaddingProperty(name, value); //toplist
-      case "barPadding" -> new PaddingProperty(name, value); //goal
-      case "innerPadding" -> new PaddingProperty(name, value); //goal
-      case "listPadding" -> new PaddingProperty(name, value); //goal
-      case "eventsPadding" -> new PaddingProperty(name, value); //horizontalevents
-      case "linePadding" -> new PaddingProperty(name, value); //horizontalevents
-      case "requesterPadding" -> new PaddingProperty(name, value); //player-info
-      case "queuePadding" -> new PaddingProperty(name, value); //player-info
-      case "rounding" -> new RoundingProperty(name, value); //goal
-      case "titleRounding" -> new RoundingProperty(name, value); //goal
-      case "itemRounding" -> new RoundingProperty(name, value); //toplist
-      case "headerRounding" -> new RoundingProperty(name, value); //toplist
-      case "outerRounding" -> new RoundingProperty(name, value); //goal
-      case "innerRounding" -> new RoundingProperty(name, value); //goal
-      case "listRounding" -> new RoundingProperty(name, value); //goal
-      case "eventsRounding" -> new RoundingProperty(name, value); //horizontalevents
-      case "lineRounding" -> new RoundingProperty(name, value); //horizontalevents
-      case "requesterRounding" -> new RoundingProperty(name, value); //player-info
-      case "queueRounding" -> new RoundingProperty(name, value); //player-info
-      case "boxShadow" -> new BoxShadowProperty(name, value); //goal
-      case "titleBoxShadow" -> new BoxShadowProperty(name, value); // goal
-      case "headerBoxShadow" -> new BoxShadowProperty(name, value); // goal
-      case "outerBoxShadow" -> new BoxShadowProperty(name, value); // goal
-      case "innerBoxShadow" -> new BoxShadowProperty(name, value); // goal
-      case "listBoxShadow" -> new BoxShadowProperty(name, value); // goal
-      case "itemBoxShadow" -> new BoxShadowProperty(name, value); // toplist
-      case "headerShadow" -> new BoxShadowProperty(name, value); // horizontalevents
-      case "eventsShadow" -> new BoxShadowProperty(name, value); // horizontalevents
-      case "lineShadow" -> new BoxShadowProperty(name, value); // horizontalevents
-      case "showTitle" -> new BooleanProperty(name, value); //goal
-      case "showLabel" -> new BooleanProperty(name, value); //goal
-      case "showHeader" -> new BooleanProperty(name, value); //toplist
-      case "showNickname" -> new BooleanProperty(name, value); //horizontalevents
-      case "showMessage" -> new BooleanProperty(name, value); //horizontalevents
-      case "showAmount" -> new BooleanProperty(name, value); //horizontalevents
-      case "hideEmpty" -> new BooleanProperty(name, value); //toplist
-      case "resetOnLoad" -> new BooleanProperty(name, value); //donationtimer
-      case "showRequester" -> new BooleanProperty(name, value); //player-info
-      case "showQueueSize" -> new BooleanProperty(name, value); //player-info
-      case "audioOnly" -> new BooleanProperty(name, value); //player-info
+      // case "titleBackgroundImage" -> new BackgroundImageProperty(name, value); //goal
+      // case "headerBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
+      // case "outerImage" -> new BackgroundImageProperty(name, value); //goal
+      // case "innerImage" -> new BackgroundImageProperty(name, value); //goal
+      // case "listBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
+      // case "itemBackgroundImage" -> new BackgroundImageProperty(name, value); //toplist
+      // case "eventsBackgroundImage" -> new BackgroundImageProperty(name, value); //horizontalevents
+      // case "lineBackgroundImage" -> new BackgroundImageProperty(name, value); //horizontalevents
+      // case "border" -> new BorderProperty(name, value); //goal
+      // case "titleBorder" -> new BorderProperty(name, value); //goal
+      // case "itemBorder" -> new BorderProperty(name, value); //toplist
+      // case "headerBorder" -> new BorderProperty(name, value); //toplist
+      // case "outerBorder" -> new BorderProperty(name, value); //goal
+      // case "innerBorder" -> new BorderProperty(name, value); //goal
+      // case "widgetBorder" -> new BorderProperty(name, value); //toplist
+      // case "listBorder" -> new BorderProperty(name, value); //toplist
+      // case "eventsBorder" -> new BorderProperty(name, value); //toplist
+      // case "lineBorder" -> new BorderProperty(name, value); //horizontalevents
+      // case "cardBorder" -> new BorderProperty(name, value); //reel
+      // case "requesterBorder" -> new BorderProperty(name, value); //player-info
+      // case "queueBorder" -> new BorderProperty(name, value); //player-info
+      // case "padding" -> new PaddingProperty(name, value); //goal
+      // case "titlePadding" -> new PaddingProperty(name, value); //goal
+      // case "itemPadding" -> new PaddingProperty(name, value); //toplist
+      // case "headerPadding" -> new PaddingProperty(name, value); //toplist
+      // case "barPadding" -> new PaddingProperty(name, value); //goal
+      // case "innerPadding" -> new PaddingProperty(name, value); //goal
+      // case "listPadding" -> new PaddingProperty(name, value); //goal
+      // case "eventsPadding" -> new PaddingProperty(name, value); //horizontalevents
+      // case "linePadding" -> new PaddingProperty(name, value); //horizontalevents
+      // case "requesterPadding" -> new PaddingProperty(name, value); //player-info
+      // case "queuePadding" -> new PaddingProperty(name, value); //player-info
+      // case "rounding" -> new RoundingProperty(name, value); //goal
+      // case "titleRounding" -> new RoundingProperty(name, value); //goal
+      // case "itemRounding" -> new RoundingProperty(name, value); //toplist
+      // case "headerRounding" -> new RoundingProperty(name, value); //toplist
+      // case "outerRounding" -> new RoundingProperty(name, value); //goal
+      // case "innerRounding" -> new RoundingProperty(name, value); //goal
+      // case "listRounding" -> new RoundingProperty(name, value); //goal
+      // case "eventsRounding" -> new RoundingProperty(name, value); //horizontalevents
+      // case "lineRounding" -> new RoundingProperty(name, value); //horizontalevents
+      // case "requesterRounding" -> new RoundingProperty(name, value); //player-info
+      // case "queueRounding" -> new RoundingProperty(name, value); //player-info
+      // case "boxShadow" -> new BoxShadowProperty(name, value); //goal
+      // case "titleBoxShadow" -> new BoxShadowProperty(name, value); // goal
+      // case "headerBoxShadow" -> new BoxShadowProperty(name, value); // goal
+      // case "outerBoxShadow" -> new BoxShadowProperty(name, value); // goal
+      // case "innerBoxShadow" -> new BoxShadowProperty(name, value); // goal
+      // case "listBoxShadow" -> new BoxShadowProperty(name, value); // goal
+      // case "itemBoxShadow" -> new BoxShadowProperty(name, value); // toplist
+      // case "headerShadow" -> new BoxShadowProperty(name, value); // horizontalevents
+      // case "eventsShadow" -> new BoxShadowProperty(name, value); // horizontalevents
+      // case "lineShadow" -> new BoxShadowProperty(name, value); // horizontalevents
+      // case "showTitle" -> new BooleanProperty(name, value); //goal
+      // case "showLabel" -> new BooleanProperty(name, value); //goal
+      // case "showHeader" -> new BooleanProperty(name, value); //toplist
+      // case "showNickname" -> new BooleanProperty(name, value); //horizontalevents
+      // case "showMessage" -> new BooleanProperty(name, value); //horizontalevents
+      // case "showAmount" -> new BooleanProperty(name, value); //horizontalevents
+      // case "hideEmpty" -> new BooleanProperty(name, value); //toplist
+      // case "resetOnLoad" -> new BooleanProperty(name, value); //donationtimer
+      // case "showRequester" -> new BooleanProperty(name, value); //player-info
+      // case "showQueueSize" -> new BooleanProperty(name, value); //player-info
+      // case "audioOnly" -> new BooleanProperty(name, value); //player-info
       case "titleAlignment" -> new AlignmentProperty(name, value); //goal
       case "titleTextAlign" -> new AlignmentProperty(name, value); //goal
       case "filledTextAlign" -> new AlignmentProperty(name, value); //goal
       case "headerAlignment" -> new AlignmentProperty(name, value); //toplist
       case "messageAlignment" -> new AlignmentProperty(name, value); //paymentalerts
       case "listAlignment" -> new AlignmentProperty(name, value); //toplist
-      case "filledTextPlacement" -> new SingleChoiceProperty(name, value); // goal
-      case "type" -> new SingleChoiceProperty(name, value); // toplist
-      case "period" -> new SingleChoiceProperty(name, value); // toplist
-      case "widgetType" -> new SingleChoiceProperty(name, value); // player-info
-      case "topsize" -> new NumberProperty(name, value); // toplist
-      case "gap" -> new NumberProperty(name, value); // toplist
-      case "eventGap" -> new NumberProperty(name, value); // horizontalevents
-      case "speed" -> new NumberProperty(name, value); // horizontal events
-      case "requiredAmount" -> new NumberProperty(name, value); // reel
-      case "perView" -> new NumberProperty(name, value); // reel
-      case "time" -> new NumberProperty(name, value); // reel
-      case "title" -> new TextProperty(name, value); //toplist
-      case "text" -> new TextProperty(name, value); //donationtimer
-      case "headerText" -> new TextProperty(name, value); //horizontal events
+      // case "filledTextPlacement" -> new SingleChoiceProperty(name, value); // goal
+      // case "type" -> new SingleChoiceProperty(name, value); // toplist
+      // case "period" -> new SingleChoiceProperty(name, value); // toplist
+      // case "widgetType" -> new SingleChoiceProperty(name, value); // player-info
+      // case "topsize" -> new NumberProperty(name, value); // toplist
+      // case "gap" -> new NumberProperty(name, value); // toplist
+      // case "eventGap" -> new NumberProperty(name, value); // horizontalevents
+      // case "speed" -> new NumberProperty(name, value); // horizontal events
+      // case "requiredAmount" -> new NumberProperty(name, value); // reel
+      // case "perView" -> new NumberProperty(name, value); // reel
+      // case "time" -> new NumberProperty(name, value); // reel
+      // case "title" -> new TextProperty(name, value); //toplist
+      // case "text" -> new TextProperty(name, value); //donationtimer
+      // case "headerText" -> new TextProperty(name, value); //horizontal events
       //
-      case "timer-end" -> new DateTimeProperty(name, value); //donaton
+      // case "timer-end" -> new DateTimeProperty(name, value); //donaton
       default -> new WidgetProperty<>(name, value);
     };
   }
